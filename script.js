85d1b59e45528902220c3d0000fbb33a55952ded
Rishon Canete <rishon.inigo@gmail.com>
2025-02-27 20:37:18 -0800

    Updates

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
-function expandContainer(element) {
-    // Remove the expanded class from any currently expanded container
-    document.querySelectorAll(".index_container").forEach(container => {
-        container.classList.remove("expanded");
-    });
+// function expandContainer(element) {
+//     // Remove the expanded class from any currently expanded container
+//     document.querySelectorAll(".index_container").forEach(container => {
+//         container.classList.remove("expanded");
+//     });
 
-    // Add the expanded class to the clicked container
-    element.classList.add("expanded");
+//     // Add the expanded class to the clicked container
+//     element.classList.add("expanded");
 
-    // Optional: Add a close function when clicking anywhere in the expanded container
-    element.onclick = function () {
-        element.classList.remove("expanded");
-        element.onclick = function () { expandContainer(element); }; // Restore original click function
-    };
-}
+//     // Optional: Add a close function when clicking anywhere in the expanded container
+//     element.onclick = function () {
+//         element.classList.remove("expanded");
+//         element.onclick = function () { expandContainer(element); }; // Restore original click function
+//     };
+// }
